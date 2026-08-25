2305b9decc8d42824efb9c9497e661f83e94ccb0
sianbir21-12spec <sianbirmaken.svkm@gmail.com>
2026-08-25 12:17:18 +0530

    feat(mobile): attach touch controls to client

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,8 +1,10 @@
 import './style.css';
 import Game from './game.ts';
+import { MobileControls } from './input/mobile-controls.ts';
 
 function main(): void {
   const game = new Game();
+  new MobileControls(game.inputController);
   game.init();
 }
 
